44adf368a5913f478cba2425b7c87b335d7a61ff
Carlinhao <carsilva.ti@gmail.com>
2024-02-02 23:49:44 -0300

    feat: Create interface IRepository and implementing concrete class Repository

---
+++ DesafioPicPay.Core/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using DesafioPicPay.Core.Models;
+
+namespace DesafioPicPay.Core.Interfaces;
+
+public interface IRepository<T> where T : Entity
+{
+    Task SaveAsync(T entity);
+    Task<T> GetByIdAsync(string id);
+}
---
+++ DesafioPicPay.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPicPay.Core.Interfaces;
+using DesafioPicPay.Core.Models;
+
+namespace DesafioPicPay.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : Entity
+{
+    public Task SaveAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetByIdAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
