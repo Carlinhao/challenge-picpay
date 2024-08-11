d307acd7860903af546a93867cc9ee51ff4915b1
Carlinhao <carsilva.ti@gmail.com>
2024-08-11 00:18:27 -0300

    feat: Creating class to configure Dependency Injection

---
+++ DesafioPicPay.Infrastructure/IoC/ServiceConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using DesafioPicPay.Core.Interfaces;
+using DesafioPicPay.Infrastructure.MessageBus;
+using DesafioPicPay.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DesafioPicPay.Infrastructure.IoC
+{
+    public static class ServiceConfiguration
+    {
+        public static IServiceCollection GetService(this IServiceCollection services)
+        {
+            services.AddDbContext<DbContextConf>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IEventBus, RabbitMqConfiguration>();
+            services.AddScoped<ITransferRepository, TransferRepository>();
+
+            return services;
+        }
+    }
+}
