6ca26e8eb81766136732a3d77f2c76f7661f26bd
Carlinhao <carsilva.ti@gmail.com>
2024-08-11 21:05:49 -0300

    feat: Creating Dtos class

---
+++ DesafioPicPay.Core/Dtos/AccountDto.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DesafioPicPay.Core.Dtos
+{
+    public class AccountDto
+    {
+        [JsonPropertyName("id")]
+        public string UserId { get; set; }
+
+        [JsonPropertyName("costumerName")]
+        public string CostumerName { get; set; }
+
+        [JsonPropertyName("balance")]
+        public decimal Balance { get; set; }
+
+        [JsonPropertyName("active")]
+        public bool Active { get; set; }
+
+        [JsonPropertyName("createDate")]
+        public DateTime CreateDate { get; set; }
+
+        [JsonPropertyName("updateDate")]
+        public DateTime UpdateDate { get; set; }
+    }
+}
---
+++ DesafioPicPay.Core/Dtos/BasePayeer.cs
@@ -0,0 +1,8 @@
+﻿namespace DesafioPicPay.Core.Dtos
+{
+    public class BasePayeer
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
