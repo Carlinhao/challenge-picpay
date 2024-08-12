9bbc3445daed54fff8a491ec26c8c095a930eec0
Carlinhao <carsilva.ti@gmail.com>
2024-08-11 21:04:19 -0300

    feat: Add classe para validação de models

--- DesafioPicPay.Core/DomainObjects/AssertionConcern.cs
+++ DesafioPicPay.Core/DomainObjects/AssertionConcern.cs
@@ -1,4 +1,6 @@
-﻿namespace DesafioPicPay.Core.DomainObjects;
+﻿using DesafioPicPay.Core.Interfaces;
+
+namespace DesafioPicPay.Core.DomainObjects;
 
 public class AssertionConcern
 {
@@ -11,10 +13,34 @@ public class AssertionConcern
             throw new DomainException(message);
         }
     }
-    
+
     public static void ValidateIfCanTransfer(decimal value, decimal accountValue, string message)
     {
         if (value < accountValue || value > 0)
+        {
+            throw new DomainException(message);
+        }
+    }
+
+    public static void ValidateIfObjectIsNull(object value, string message)
+    {
+        if (value is null)
+        {
+            throw new DomainException(message);
+        }
+    }
+
+    public static void ValidateIfValueTransferIsZero(decimal value, string message)
+    {
+        if (value <= 0)
+        {
+            throw new DomainException(message);
+        }
+    }
+
+    public static void ValidateIfTypeYser(char value, string message)
+    {
+        if (!value.Equals('F') || !value.Equals('f') || !value.Equals('J') || !value.Equals('j'))
         {
             throw new DomainException(message);
         }
